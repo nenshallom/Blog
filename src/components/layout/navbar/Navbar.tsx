ddcf49a4b6956ec6d85cf2f76dac8954df10bc30
nenshallom <sshallom543@gmail.com>
2025-07-30 18:45:11 +0100

    fix: logo responsiveness

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -30,10 +30,10 @@ export default function Navbar() {
           {/* Logo */}
           <Link to="/" className="flex items-center">
             <div className="flex items-center rounded-lg overflow-hidden font-bold text-sm sm:text-base md:text-lg lg:text-xl shadow transition-colors duration-300">
-              <span className="px-1 sm:pl-1 py-1 bg-[#184E59] dark:bg-green-500 text-white transition-colors duration-300">
+              <span className="px-1 sm:pr-0 py-1 bg-[#184E59] dark:bg-green-500 text-white transition-colors duration-300">
                 CodeAnd
               </span>
-              <span className="pr-2 sm:px-1 py-1 bg-green-500 dark:bg-[#184E59] text-white transition-colors duration-300">
+              <span className="pr-2  py-1 bg-green-500 dark:bg-[#184E59] text-white transition-colors duration-300">
                 Cultivate
               </span>
             </div>
