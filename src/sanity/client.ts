9e5a2aa8dc86c6aca55c25643093709a9194b6f2
nenshallom <sshallom543@gmail.com>
2025-08-20 13:56:49 +0100

    fix: deployment issue

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@sanity/client';
 
 export const sanity = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
+  projectId: import.meta.env.SANITY_PROJECT_ID as string,
+  dataset: import.meta.env.SANITY_DATASET as string,
   apiVersion: '2023-08-01',
   useCdn: true, // `false` if you want to ensure fresh data
 });
