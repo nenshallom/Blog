7802d2a96f40fa89dcd7173705f5ab7fa3150c07
nenshallom <sshallom543@gmail.com>
2025-09-20 19:49:16 +0100

    feat: Google Analytics

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,7 +1,7 @@
 // src/utils/analytics.ts
 import ReactGA from 'react-ga4';
 
-const MEASUREMENT_ID = "G-XXXXXXXXXX"; // <-- Paste your Measurement ID here
+const MEASUREMENT_ID = "G-GQ54FPG7ZW"; // <-- Paste your Measurement ID here
 
 export const initGA = () => {
   if (process.env.NODE_ENV === "production") {
